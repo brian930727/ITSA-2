62b31d7364238910664a8db6b64c174d4fe764b4
brian930727 <73778642+brian930727@users.noreply.github.com>
2023-03-10 14:01:40 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+#include <iomanip>  
+using namespace std;  
+  
+int main() {  
+    int miles;  
+    double kilometers;  
+    cin >> miles;  
+    kilometers = miles * 1.6;  
+    cout << fixed << setprecision(1) << kilometers << endl;  
+    return 0;  
+}  
